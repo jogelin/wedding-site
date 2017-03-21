b1db950f5fa3abb81d133b374348ef391a503f6f
Jonathan Gelin <jonathan.gelin@sodexo.com>
2017-03-21 09:12:32 +0100

    add section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { environment } from '../environments/environment';
+
 
 @Component({
     selector: 'wg-root',
@@ -6,13 +8,20 @@ import {Component} from '@angular/core';
     
     `],
     template: `        
-        <!--<div class="h-100" ngsRevealSet [ngsSelector]="'.row'"> -->
-            <!--<wg-sections-page></wg-sections-page>-->
-        <!--</div>-->
-
-UNDER CONSTRUCTION
+        <div *ngIf="!unConstruction" class="h-100" ngsRevealSet [ngsSelector]="'.row'"> 
+            <wg-sections-page></wg-sections-page>
+        </div>        
+        <div *ngIf="unConstruction"> 
+            UNDER CONSTRUCTION
+        </div>
         
     `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    unConstruction:boolean = true;
+
+    ngOnInit(): void {
+        this.unConstruction = environment.production;
+    }
 }
--- src/app/section/sections-page.component.ts
+++ src/app/section/sections-page.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { environment } from '../../environments/environment';
-import {Observable} from "rxjs";
+import {environment} from '../../environments/environment';
 
 
 @Component({
