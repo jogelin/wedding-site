2a13690587a8998aa86bde24772d5b8688340685
Jonathan Gelin <gelin.jonathan@gmail.com>
2017-04-05 22:28:42 +0200

    add auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import * as guest  from "./guest/guest.actions";
 import * as kadolog  from "./kadolog/kadolog.actions";
 import {Store} from "@ngrx/store";
 import * as fromRoot from "./app.reducer";
+import {AngularFire, AuthMethods, AuthProviders} from "angularfire2";
 
 
 
@@ -26,12 +27,17 @@ export class AppComponent implements OnInit {
 
     unConstruction:boolean = true;
 
-    constructor(private _store: Store<fromRoot.State>) {
+    constructor(private _store: Store<fromRoot.State>, public _af: AngularFire) {
 
     }
 
     ngOnInit(): void {
         this.unConstruction = environment.production;
+
+        this._af.auth.login({
+            provider: AuthProviders.Anonymous,
+            method: AuthMethods.Anonymous
+        });
 
         this._store.dispatch(new guest.LoadListAction());
         this._store.dispatch(new kadolog.LoadListAction());
